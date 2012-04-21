3bc7ab6b610aedea2f065a1134a472a4cdb3af4e
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 16:49:58 -0500

    More fixes

--- recipes/bourbon.rb
+++ recipes/bourbon.rb
@@ -1,5 +1,13 @@
 gem 'bourbon'
 
+after_bundler do
+  generate("bootstrap:install")
+  run("rm app/views/layouts/application.html.erb")
+  generate("bootstrap:layout application fixed")
+  
+  # TODO move app/asseets/stylesheeets/application.css to application.css.scss
+end
+
 __END__
 
 name: Bourbon
--- recipes/devise.rb
+++ recipes/devise.rb
@@ -4,7 +4,7 @@ after_bundler do
   generate 'devise:install'
   generate 'devise administrator'
   
-  inject_into_file "config/environment/development.rb", 
+  inject_into_file "config/environments/development.rb", 
                   :after => "config.action_mailer.raise_delivery_errors = false" do <<-RUBY
       # Default host for mailer routes
       config.action_mailer.default_url_options = { :host => 'localhost:3000' }
