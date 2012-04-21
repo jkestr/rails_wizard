f0b79f3b8de48baa3481c2851f106e4f9199c02d
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 16:45:16 -0500

    Cleaning things up a little more

--- recipes/devise.rb
+++ recipes/devise.rb
@@ -3,6 +3,14 @@ gem 'dm-devise'
 after_bundler do
   generate 'devise:install'
   generate 'devise administrator'
+  
+  inject_into_file "config/environment/development.rb", 
+                  :after => "config.action_mailer.raise_delivery_errors = false" do <<-RUBY
+      # Default host for mailer routes
+      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+   RUBY
+  end
+  
 end
 
 __END__
