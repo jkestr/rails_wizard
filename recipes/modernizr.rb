6dfa97ac15c7d56166664225037f57fdfa4bfc18
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 16:56:31 -0500

    Fixing mondernizr insertation

--- recipes/modernizr.rb
+++ recipes/modernizr.rb
@@ -2,8 +2,8 @@ gem 'modernizr_rails'#, :require => 'modernizr-rails'
 
 after_bundler do
   insert_into_file("app/assets/javascripts/application.js", 
-                   "//= require modernizr", 
-                   :before => /\/\/= require jquery/)
+                   "//= require modernizr\n", 
+                   :before => /\/\/= require jquery\n/)
 end
 
 
