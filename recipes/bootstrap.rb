3b3c543915d8d8b92cafd0cb12468acc831e8dbe
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 15:53:35 -0500

    Updated bootstrap recipie

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -1,7 +1,10 @@
+# https://github.com/seyhunak/twitter-bootstrap-rails
 gem 'twitter-bootstrap-rails'
 
 after_bundler do
   generate("bootstrap:install")
+  run("rm app/views/layouts/application.html.erb")
+  generate("bootstrap:layout application fixed")
 end
 
 __END__
