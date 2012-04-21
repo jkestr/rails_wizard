8c22c11909e14e723405f67eca56ec13f99bfe8d
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 16:31:41 -0500

    Cleaned up welcome & clean

--- recipes/welcome.rb
+++ recipes/welcome.rb
@@ -1,5 +1,7 @@
 after_bundler do
   run("rm public/index.html")
+  run("mkdir -p app/views/application")
+  remove_file 'public/images/rails.png'
   
   create_file "app/views/application/index.html.haml", <<-RUBY
     %section
