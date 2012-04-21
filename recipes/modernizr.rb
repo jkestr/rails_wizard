d893de20245621164ee13ed09e0b008f21d7df54
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 16:24:30 -0500

    Fixed typo

--- recipes/modernizr.rb
+++ recipes/modernizr.rb
@@ -3,7 +3,7 @@ gem 'modernizr_rails'#, :require => 'modernizr-rails'
 after_bundler do
   insert_into_file("app/assets/javascripts/application.js", 
                    "//= require modernizr", 
-                   :before => ///= require jquery/)
+                   :before => /\/\/= require jquery/)
 end
 
 
