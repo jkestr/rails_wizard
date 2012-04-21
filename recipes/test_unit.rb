599a43489f28e9e6a2897eb7c2adc7d21f31c128
Jacob Basham <jacob@paperpigeons.net>
2012-04-21 15:40:52 -0500

    Reverted back to original test unit

--- recipes/test_unit.rb
+++ recipes/test_unit.rb
@@ -1,36 +1,11 @@
-set :application, "APPLICATION_NAME"
-set :user,        "SSH_USER_NAME"
-set :domain,      "DOMAIN"
-set :repository,  "GIT_REPOSITORY"
-set :rails_env,   "production"
+# No additional code required.
 
-namespace :vlad do
-  
-  task :deploy => [
-    :'maintenance:on',
-    :update,
-    :bundle,
-    :migrate,
-    :'maintenance:off',
-    :cleanup
-  ]  
-  
-  remote_task :bundle do
-    run(\"cd #{current_directory} && RAILS_ENV=#{rails_env} rake bundle install --deployment\")
-  end
-  
-  remote_task :migrate do
-    run(\"cd #{current_directory} && RAILS_ENV=#{rails_env} rake db:autoupgrade\")
-  end
-  
-  namespace :maintenance do
-    remote_task :on, :roles => :web do
-      run(\"cp #{current_path}/public/503.html #{current_path}/public/system/maintenance.html\")
-    end
+__END__
 
-    remote_task :off, :roles => :web do
-      run(\"rm -f #{current_path}/public/system/maintenance.html\")
-    end                                                                                                                                                                                         
-  end
-  
-end
+name: Test::Unit
+description: "Utilize the default Rails test facilities."
+author: mbleigh
+
+exclusive: unit_testing
+category: testing
+tags: [defaults]
