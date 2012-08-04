a6e6cc03c85075c3305a67c01622b2f45eb09693
Jacob Basham <jacob@paperpigeons.net>
2012-08-04 09:19:49 -0500

    Updated Google Analytics recipe

---
+++ recipes/google-analytics.rb
@@ -0,0 +1,21 @@
+gem 'rack-google-analytics'
+
+after_bundler do
+  analytics = %Q{
+  # Enable Google Analytics tracking
+  # Replace UA- with clients property id from Google Analytics
+  # config.middleware.use("Rack::GoogleAnalytics", :tracker => "UA-0000000-1")
+  }
+
+  insert_into_file("config/environments/production.rb", 
+                   analytics, 
+                   :after => /config.serve_static_assets = false/)
+end
+
+__END__
+
+name: Google Analytics
+description: Add Google analytics in the stack
+author: idk
+
+category: javascript
