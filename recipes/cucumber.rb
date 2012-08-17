93df3d3957ecb55c890706f13d663ced0534a23d
Jacob Basham <jacob@killswitchcollective.com>
2012-08-17 13:07:46 -0500

    Added database cleaner to cucumber receipe

--- recipes/cucumber.rb
+++ recipes/cucumber.rb
@@ -1,5 +1,6 @@
 gem 'cucumber-rails', :group => [:test]
 gem 'capybara', :group => [:test]
+gem 'database_cleaner', :group => [:test]
 
 after_bundler do
   generate "cucumber:install --capybara#{' --rspec' if recipes.include?('rspec')}#{' -D'}"
